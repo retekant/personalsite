eab8a3a13d06b407e4eea8bc458c703cbd40b2bc
retekant <retekant@gmail.com>
2025-04-22 21:38:53 -0700

    icons working/template

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,8 +2,8 @@
 export default function Home() {
   
   return (
-    <div class="h-2/3 mx-auto bg-[#7c7dd4] flex justify-center items-center py-7">
-      <div class="flex flex-col text-white bg-[#474885] rounded-lg font-semibold p-5 gap-5">
+    <div class="h-2/3 mx-auto bg-[#7c7dd4] flex flex-col justify-center items-center py-7 gap-6">
+      <div class="flex flex-col text-white bg-[#474885] rounded-lg font-semibold p-5 gap-5 w-4/12">
 
           <div class='flex flex-row justify-between'>
             <div>
@@ -19,7 +19,26 @@ export default function Home() {
         <p>An upcoming Computer Science and Engineering Student studying at _____</p>
       </div>
         
-      
+        <div class=" text-white bg-[#474885] rounded-lg font-semibold p-5 gap-5 w-4/12
+        text-center flex flex-col gap2">
+         <h1 class="font-bold text-2xl "> Technologies</h1>
+        <div class='w-full flex flex-row'>
+          <ul class='flex flex-row items-center'>
+            <li class='flex flex-row items-center'>
+            <img src="/logo.svg" width="48" height="48" alt="the Fresh by deno logo"/>
+            <p>Fresh</p>
+            </li>
+            
+            <li>
+
+            </li>
+            <li>
+
+            </li>
+
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
