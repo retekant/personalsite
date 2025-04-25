e9ae180ad1a061bc10d8cd7c49e365ada91f7453
retekant <retekant@gmail.com>
2025-04-24 19:46:52 -0700

    updating hp + github

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,10 +4,11 @@ export default function Home() {
   return (
     <div class="h-screen mx-auto bg-[#7c7dd4] flex flex-col justify-center items-center py-7 gap-6">
 
-      <div class="flex flex-col text-white bg-[#474885] rounded-lg font-semibold px-5 gap-5 
-      w-6/12  h-[45%]">
-        
-          <div class='flex flex-row justify-between'>
+      <div class=" text-white bg-[#474885] rounded-lg font-semibold  
+      w-5/12  h-[45%] flex items-center justify-center">
+
+        <div class='flex flex-col p-8 gap-5 '>
+        <div class='flex flex-row justify-between'>
             <div>
             <h1 class="font-bold text-6xl">hi</h1>
             <h3 class="text-3xl">I'm Connor</h3>
@@ -19,10 +20,18 @@ export default function Home() {
         
         
         <p>An upcoming Computer Science and Engineering Student studying at _____</p>
-        
+
+        <a href="https://github.com/retekant" class='text-white' >
+         <img src="/github.svg" alt="GitHub Logo" width="32" height="32" class="invert" />
+        </a>
+
+
+        </div>
+
+
       </div>
         
-        <div class=" text-white bg-[#474885] rounded-lg font-semibold p-5 gap-5 w-4/12
+        <div class=" text-white bg-[#474885] rounded-lg font-semibold p-8 gap-5 w-4/12
         text-center flex flex-col"> 
          <h1 class="font-bold text-xl "> Technologies</h1>
         <div class='w-full grid grid-cols-3 gap-x-8 gap-y-4 justify-items-center text-lg'>
